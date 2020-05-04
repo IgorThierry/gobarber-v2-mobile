3930b76fb8a093b923200c28d359948e455dbfab
Igor Thierry <igorthierry15@gmail.com>
2020-05-04 15:57:30 -0300

    feat: Input & Button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
+
+import Input from '../../Components/Input';
+import Button from '../../Components/Button';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-amil" />
+      <Input name="password" icon="lock" placeholder="Senha" secureTextEntry />
+      <Button
+        onPress={() => {
+          console.log('adof');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
