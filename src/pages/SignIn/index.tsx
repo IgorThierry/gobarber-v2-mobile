ac7dbcf594813db9bdec791e4aeaf7a05048f7a3
Igor Thierry <igorthierry15@gmail.com>
2020-05-05 12:56:45 -0300

    feat: signIn screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,29 +1,105 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useEffect, useCallback, useState } from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../Components/Input';
 import Button from '../../Components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
-  return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
+  const [showButton, setShowButton] = useState(true);
 
-      <Input name="email" icon="mail" placeholder="E-amil" />
-      <Input name="password" icon="lock" placeholder="Senha" secureTextEntry />
-      <Button
-        onPress={() => {
-          console.log('adof');
-        }}
+  const hideCreateAccountButton = useCallback(() => {
+    setShowButton(false);
+  }, []);
+  const showCreateAccountButton = useCallback(() => {
+    setShowButton(true);
+  }, []);
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      Keyboard.addListener('keyboardDidShow', hideCreateAccountButton);
+      Keyboard.addListener('keyboardDidHide', showCreateAccountButton);
+
+      return () => {
+        Keyboard.removeListener('keyboardDidShow', hideCreateAccountButton);
+        Keyboard.removeListener('keyboardDidHide', showCreateAccountButton);
+      };
+    }
+    return undefined;
+  }, [hideCreateAccountButton, showCreateAccountButton]);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-amil" />
+            <Input
+              name="password"
+              icon="lock"
+              placeholder="Senha"
+              secureTextEntry
+            />
+            <Button
+              onPress={() => {
+                console.log('adof');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword
+              onPress={() => {
+                console.log('adof');
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {showButton && (
+        <CreateAccountButton
+          onPress={() => {
+            console.log('adof');
+          }}
+        >
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      )}
+    </>
   );
 };
 
